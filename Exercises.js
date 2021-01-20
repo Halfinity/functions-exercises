ac1ce922fdd0cbeb2c2c58f52589e9a131fb1a7c
Halfinity <ofirhalfin13@gmail.com>
2021-01-20 12:50:53 +0200

    Half Answers

--- Exercises.js
+++ Exercises.js
@@ -1,34 +1,56 @@
 //Question 1
 function myReverse(str) {
-  // your code here
-  return "";
-}
+  {
+      str = str + "";
+      return str.split("").reverse().join("");
+    }
+  }
 
 //Question 2
 function allCombinations(str) {
-  let combinations = [];
-  // your code here
-
-  //
+  list = new Array();
+  for(i=0;i<s.length;i++) {
+      for(j=i+1;j<s.length+1;j++) {
+          list.push(s.slice(i, j));
+      }
   return combinations;
+    }
 }
+
 
 //Question 3
 function allCaps(str) {
-  // your code here
-  return "";
+  words = str.toLowerCase().split(' ');
+
+  for(let i = 0; i < words.length; i++) {
+       let letters = words[i].split('');
+       letters[0] = letters[0].toUpperCase();
+       words[i] = letters.join('');
+  }
+  return words.join(' ');
 }
 
 //Question 4
-function myPower(x, n) {
-  // your code here
-  return "";
+function myPower(x, n) 
+  {
+    let power = 1;
+    for (let i =1; i <= n; i++)
+    {
+      power = x * power;        
+    }
+    return power;
 }
+
 
 //Question 5
 function getFirstNotRepeating(str) {
-  // your code here
-  return "";
+  for (let i = 0; i < str.length; i++) {
+    let first = str[i];
+    if (str.indexOf(first) == i && str.indexOf(first, i + 1) == -1) {
+      return first;
+    }
+  }
+  return result;
 }
 
 //Question 6 (Bonus)
